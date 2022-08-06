7279146edcbc66911a6577d9b4b7c5083e490cdb
209 <209@academicos.cesde.edu.co>
2022-08-05 20:07:37 -0500

    nrc1 se crea index y salida por consola

---
+++ main.py
@@ -0,0 +1,19 @@
+from tkinter.tix import Tree
+
+
+print("Hola mundo")
+
+#Comentario de linea
+
+'''
+Comentario de Bloque
+:D
+'''
+
+#No necesitas definir las variables
+#Las variables no pueden empezar en mayuscula,solo las funciones
+
+nombre='Santiago'
+Edad=21
+estatura=1.85
+estudianteDeCesde=True
